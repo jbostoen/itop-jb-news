0bf53f981dc1154088afa1f572d8d9c0b8ec7e2c
Jeffrey Bostoen <support@jeffreybostoen.be>
2024-06-17 16:28:10 +0200

    * Bump version before release.

--- src/Core/NewsRoomHelper.php
+++ src/Core/NewsRoomHelper.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (c) 2019-2024 Jeffrey Bostoen
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2.7.240527
+ * @version     2.7.240617
  *
  */
 
@@ -20,12 +20,10 @@ use \MetaModel;
 use \User;
 use \UserRights;
 use \utils;
-use \WebPage;
 
 // iTop classes
 use \ThirdPartyNewsRoomMessage;
 use \ThirdPartyNewsRoomMessageTranslation;
-use \ThirdPartyMessageReadStatus;
 
 // Custom classes
 use \jb_itop_extensions\NewsProvider\NewsRoomWebPage;
