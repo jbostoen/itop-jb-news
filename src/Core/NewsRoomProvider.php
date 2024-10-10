509f4b1b80aeed1591a8a093cef2cb12c9c08a7b
Jeffrey Bostoen <support@jeffreybostoen.be>
2024-10-10 17:10:01 +0200

    * Bump version number.

--- src/Core/NewsRoomProvider.php
+++ src/Core/NewsRoomProvider.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (c) 2019-2024 Jeffrey Bostoen
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2.7.240828
+ * @version     3.2.241010
  *
  * Definition of NewsRoomProvider.
  */
